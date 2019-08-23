9d07a93dafd578f45d4390d7b3da2612e66f0cad
Jcarlos-rl <carlos_-619@hotmail.com>
2019-08-22 22:17:55 -0500

    Nueva imagen

--- js/materialize.js
+++ js/materialize.js
@@ -1,73 +1,73 @@
 $(document).ready(function(){
-    $('.slider').slider({
-        height: $(window).height(),
-        indicators: false,
-    });
-});
-
-$(document).ready(function(){
     $('.sidenav').sidenav();
-});
-
-$(document).ready(function(){
-    $('.carousel').carousel({
-        dist: 0,
-        indicators: true,
+    $('.fixed-action-btn').floatingActionButton();
+    $('.slider').slider({
+        height: 190
     });
-});
-
-setInterval(function(){
-    $('.carousel').carousel('next');
-}, 6000);
-
-$('.carousel.carousel-slider').carousel({
-    fullWidth: true,
-    indicators: true
-});
-
-$(document).ready(function(){
     $('.tabs').tabs();
-});
 
-$(document).ready(function(){
-    $('.modal').modal();
-});
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-      document.body.style.backgroundColor = "yellow";
-    } else {
-     document.body.style.backgroundColor = "pink";
+    setTimeout(function(){
+        $(".logo-letter").css({"display":"block","transition":"3s"})
+    },6500)
+    var x = window.matchMedia("(min-width: 993px)")
+    myFunction(x)
+    function myFunction(x) {
+        if (x.matches) {
+            $("body").mousemove(function(e) {
+                parallaxIt(e, ".fondo", -50);
+            });
+            function parallaxIt(e, target, movement) {
+                var $this = $("body");
+                var relX = e.pageX - $this.offset().left;
+                var relY = e.pageY - $this.offset().top;
+              
+                TweenMax.to(target, 1, {
+                  x: (relX - $this.width() / 4) / $this.width() * movement,
+                  y: (relY - $this.height() / 4) / $this.height() * movement
+                });
+            }
+        }
     }
-}
 
-$(document).ready(function(){
     var flag=false;
     var scroll;
-    var x = window.matchMedia("(max-width: 600px)");
     $(window).scroll(function(){
         scroll=$(window).scrollTop();
-        if(scroll > 100){
+        if(scroll > 50){
             if(!flag){
-                $(".logo-nav").css({"margin-top": "5px","width": "100px","display":"none"});
-                $("a.sidenav-trigger").css({"display":"block"});
-                $("ul.menu").css({"display":"none"});
-                $("nav.nav-color").css({"background-color":"#2e2e2e"});
-                $(".logo-text").css({"margin-top": "5px"});
+                $("nav").css({"background-color": "#E9EBF0","box-shadow":"0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2)"});
                 flag=true;
             }
         }else{
             if(flag){
-                $(".logo-nav").css({"width": "250px","transition":"0.5s","display":"block"});
-                if(x.matches){
-                    $("a.sidenav-trigger").css({"display":"block"});
-                }else{
-                    $("a.sidenav-trigger").css({"display":"none"});
-                }
-                $("ul.menu").css({"display":"block"});
-                $("nav.nav-color").css({"background-color":"transparent"});
-                $(".logo-text").css({"margin-top": "105px", "transition":"0.5s"});
+                $("nav").css({"background-color": "transparent","box-shadow":"0 0 0 0 rgba(0,0,0,0.14), 0 0 0 0 rgba(0,0,0,0.12), 0 0 0 0 rgba(0,0,0,0.2)"});
                 flag=false;
             }
         }
     });
 });
+function tabs(evt, cityName, hr) {
+    $(".header h1").css({"font-size":"3rem","transition":"0.2s"});
+    $(".header h5,.center h6,.header div,.header p,.container h6,.container i,div.spacing1").css({"display":"none","transition":"0.2s"});
+    $(".container-service").css({"margin":"0 auto","max-width":"1280px","width":"50%"});
+    $(".servicios svg").css({"width":"50px"});
+    $(".simpsa, .ali, .cloud").css({"width":"100%","height":"90px"});
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    hrs = document.getElementsByClassName("hr");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    for (i = 0; i < hrs.length; i++) {
+        hrs[i].style.display = "none";
+    }
+    active = document.getElementsByClassName("img");
+    for (i = 0; i < active.length; i++) {
+        active[i].className = active[i].className.replace(" active", "");
+        console.log(active[i]);
+    }
+    document.getElementById(cityName).style.display = "block";
+    document.getElementById(hr).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+      
